99caf55b1a2df6f66f52765bb0d0de4e17ce3fbe
Tetiana Horobets <tetiana.horobets@gmail.com>
2020-04-25 13:24:06 -0700

    Show stats

--- src/QuestionPage.js
+++ src/QuestionPage.js
@@ -4,12 +4,32 @@ import {Redirect} from 'react-router-dom';
 
 class QuestionPage extends React.Component {
 
+  hasVoted(question) {
+    return question.optionOne.votes.includes(this.props.selectedUser)
+      || question.optionTwo.votes.includes(this.props.selectedUser)
+  }
+
+  renderStats(question) {
+    const optionOneVotes = question.optionOne.votes.length;
+    const optionTwoVotes = question.optionTwo.votes.length;
+    const totalVotes = optionOneVotes + optionTwoVotes;
+
+    return <div>
+      Option one: {optionOneVotes} / {totalVotes}<br/>
+      Option two: {optionTwoVotes} / {totalVotes}
+    </div>
+  }
+
   render() {
     if (!this.props.selectedUser) {
       return <Redirect to={'/signin'}/>
     }
 
     const question = this.props.questions[this.props.questionId];
+
+    if (this.hasVoted(question)) {
+      return this.renderStats(question)
+    }
 
     return <div>
       <h1>Would you rather?</h1>
