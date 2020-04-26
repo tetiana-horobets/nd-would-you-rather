7901c52647a24658ce4c5960075daf3f30ebcc0d
Tetiana Horobets <tetiana.horobets@gmail.com>
2020-04-25 18:24:34 -0700

    Show leaderboard

--- src/LeaderboardPage.js
+++ src/LeaderboardPage.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default class LeaderboardPage extends React.Component {
+class LeaderboardPage extends React.Component {
+
+  getStats(user) {
+    const questions = user.questions.length;
+    const answers = Object.values(user.answers).length;
+
+    return {
+      questions: questions,
+      answers: answers,
+      score: questions + answers
+    }
+  }
 
   render() {
-    return <h1>Leaderboard page</h1>
+    const users = Object.values(this.props.users)
+      .map(user => Object.assign({}, user, {stats: this.getStats(user)}))
+      .sort((user1, user2) => user2.stats.score - user1.stats.score)
+      .map(user => <div key={user.id}>{user.name}
+          questions: {user.stats.questions}
+          answers: {user.stats.answers}
+          score: {user.stats.score}
+        </div>);
+
+    return <div>{users}</div>
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    users: state.users,
+  }
+}
+
+const mapDispatchToProps = {}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LeaderboardPage);
