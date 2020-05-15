2955d6b94cf39874976fb6557e9a47f6ebb851c4
tetiana <tetiana.horobets@gmail.com>
2020-05-14 17:59:41 -0700

    Add navbar to login

--- src/SigninPage.js
+++ src/SigninPage.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Navbar from './components/Navbar.js';
 
 class SigninPage extends React.Component {
 
@@ -19,21 +20,24 @@ class SigninPage extends React.Component {
     }
     const users = Object.entries(this.props.users)
       .map(entry => <Dropdown.Item key={entry[1].id} onClick={() => this.selectUser(entry[1].id)}>{entry[1].name}</Dropdown.Item>);
-    return <Container>
-      <Card style={{ width: '22rem', margin: '1em auto'}}>
-        <Card.Body>
-          <Card.Title>Welcome to Would You Rather!</Card.Title>
-          <div>
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Select user
-              </Dropdown.Toggle>
-              <Dropdown.Menu>{users}</Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </Card.Body>
-      </Card>
-    </Container>
+    return <div>
+       <Navbar/>
+       <Container>
+        <Card style={{ width: '22rem', margin: '1em auto'}}>
+          <Card.Body>
+            <Card.Title>Welcome to Would You Rather!</Card.Title>
+            <div>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Select user
+                </Dropdown.Toggle>
+                <Dropdown.Menu>{users}</Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
   }
 }
 
